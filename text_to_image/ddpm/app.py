45e1e9e470704055362d32ecee8b3a6ad6ab9a2e
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-10-29 08:39:35 -0300

    probando nuevos modelos

--- text_to_image/ddpm/app.py
+++ text_to_image/ddpm/app.py
@@ -10,7 +10,8 @@ os.environ["HF_HOME"] = relative_path
 ## carga el modelo
 from diffusers import DDPMPipeline
 
-prompt = "cat running"
+prompt = "a close up of a person with pink hair, inspired by Lois van Baarle, trending on Artstation, digital art, girl with short white hair, in style of cyril rolando, night color, rob rey and kentarõ miura style, concept art, mdjrny-v4 style"
+
 ddpm = DDPMPipeline.from_pretrained("google/ddpm-cat-256", use_safetensors=True, from_tf=True,
                                      prompt=prompt)
 image = ddpm(num_inference_steps=1000).images[0]
